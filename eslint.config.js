e0ffa679ce7114ef8ae30b0caf26c4413b8a3cf5
ndodanliesarj <nizameddin.dodanli@esarj.com>
2025-09-02 17:46:08 +0300

    Update ESLint configuration comments to clarify usage guidelines

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,8 @@
-import esarjConfig from '@esarj/eslint-config/ts';
-
-export default esarjConfig;
+/**
+ * This project uses the default Esarj ESLint configuration.
+ * 
+ * You should not modify or override this configuration unless you have an absolutely critical and well-justified reason.
+ * If you must customize the ESLint rules, you may import `@esarj/eslint-config/ts` and extend it in this file.
+ * However, we strongly recommend sticking with the default configuration to ensure consistency across all Esarj projects.
+ */
+export { default } from '@esarj/eslint-config/ts';
