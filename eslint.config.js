5437664b5b7fff1aeb78dfcbce76f18570b08674
Dogan Golcuk <dogan.golcuk@esarj.com>
2025-10-08 22:34:31 +0300

    Refactor ESLint and Jest configuration files to improve clarity and maintain consistency

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 /**
  * This project uses the default Esarj ESLint configuration.
- * 
+ *
  * You should not modify or override this configuration unless you have an absolutely critical and well-justified reason.
  * If you must customize the ESLint rules, you may import `@esarj/eslint-config/ts` and extend it in this file.
  * However, we strongly recommend sticking with the default configuration to ensure consistency across all Esarj projects.
  */
+// eslint-disable-next-line import-x/no-default-export
 export { default } from '@esarj/eslint-config/ts';
--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,23 @@
+// eslint-disable-next-line import-x/no-default-export
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'jest-environment-node',
-  extensionsToTreatAsEsm: ['.ts'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/'],
-  coverageDirectory: 'test/.coverage/',
-  collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
-  moduleNameMapper: {
-    '^(\\.\\.?\\/.+)\\.jsx?$': '$1'
-  },
-  transform: {
-    '^.+\\.[jt]sx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-        // tsconfig: '<rootDir>/tsconfig.test.json',
-      },
-    ],
-  },
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'jest-environment-node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/lib/'],
+    coverageDirectory: 'test/.coverage/',
+    collectCoverageFrom: ['src/**/*.ts', 'src/**/*.tsx'],
+    moduleNameMapper: {
+        '^(\\.\\.?\\/.+)\\.jsx?$': '$1'
+    },
+    transform: {
+        '^.+\\.[jt]sx?$': [
+            'ts-jest',
+            {
+                useESM: true
+                // tsconfig: '<rootDir>/tsconfig.test.json',
+            }
+        ]
+    }
 
 };
